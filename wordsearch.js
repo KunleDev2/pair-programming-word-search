32649a041a0c8693966309b6bf0ba98cc19a4aa4
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-11-10 11:31:25 -0600

    complete task

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,15 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    const verticalJoin = letters[0].map((_, i) => letters.map(row => row[i]).join(''));
+    for (const l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
